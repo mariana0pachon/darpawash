20cf2ec23201bb1ae62f01fbe88667d10a977018
mariana0pachon <mpachonpuentes@wpi.edu>
2018-12-23 20:58:56 -0500

    Updates

---
+++ precache-manifest.94283006805d07b07bac2c8199d54ce8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "166437220dd3254ca62cfb398fca2253",
+    "url": "/darpawash/static/media/icon-4.16643722.svg"
+  },
+  {
+    "revision": "b7e56e3a52915ec214aa",
+    "url": "/darpawash/static/css/main.959d54e7.chunk.css"
+  },
+  {
+    "revision": "06a8b739b3c9c56b7955",
+    "url": "/darpawash/static/js/1.06a8b739.chunk.js"
+  },
+  {
+    "revision": "a16a39085654a0043ba8",
+    "url": "/darpawash/static/js/runtime~main.a16a3908.js"
+  },
+  {
+    "revision": "a3a5dfcd34efc94f5fc53ccb6316e06b",
+    "url": "/darpawash/static/media/icon-1.a3a5dfcd.svg"
+  },
+  {
+    "revision": "29b4ca3c396285a1fa3619460f2a7dc4",
+    "url": "/darpawash/static/media/icon-2.29b4ca3c.svg"
+  },
+  {
+    "revision": "4c8d2bc6bee8f3e245214fadeae2f5df",
+    "url": "/darpawash/static/media/icon-3.4c8d2bc6.svg"
+  },
+  {
+    "revision": "b7e56e3a52915ec214aa",
+    "url": "/darpawash/static/js/main.b7e56e3a.chunk.js"
+  },
+  {
+    "revision": "b85845d353f5a1320bf9c5d558f57019",
+    "url": "/darpawash/static/media/icon-5.b85845d3.svg"
+  },
+  {
+    "revision": "06e798b9a481bfcf197a8cda2a2657bc",
+    "url": "/darpawash/static/media/icon-6.06e798b9.svg"
+  },
+  {
+    "revision": "88be7315113f23df573013b96fd9ee17",
+    "url": "/darpawash/static/media/diagram1.88be7315.jpg"
+  },
+  {
+    "revision": "61cc818a71000aef6b77a1707e5a872a",
+    "url": "/darpawash/static/media/diagram2.61cc818a.jpg"
+  },
+  {
+    "revision": "fb859c95391b29fd5c3e4db78f3ed095",
+    "url": "/darpawash/static/media/diagram3.fb859c95.jpg"
+  },
+  {
+    "revision": "3289bcc5f2745e8492ba5a76f6aafcc4",
+    "url": "/darpawash/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/darpawash/precache-manifest.94283006805d07b07bac2c8199d54ce8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/darpawash/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
